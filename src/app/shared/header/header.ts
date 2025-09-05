7c39bc0c7814616754feb373eeff77387913848d
NMakarevich <nikoladze529@mail.ru>
2025-09-06 00:35:41 +0300

    refactor: refactor header layout

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,15 +1,15 @@
-import { ScreenSmallDirective } from './directives/screen-small-directive';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { ScreenService } from '../../core/screen-service';
+import { ChangeDetectionStrategy, Component, inject, OnDestroy, signal } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 import { MenuService } from './menu/services/menu-service';
 import { MenuItems } from './menu/data/menu-data';
 import { Menu } from './menu/menu';
-import { ScreenLargeDirective } from './directives/screen-large-directive';
 import { RouterLink } from '@angular/router';
+import { LARGE_SCREEN_BREAKPOINT } from '../constants/constants';
+import { AuthIcon } from '../ui/auth-icon/auth-icon';
 
 @Component({
   selector: 'app-header',
-  imports: [Menu, ScreenLargeDirective, ScreenSmallDirective, RouterLink],
+  imports: [Menu, RouterLink, AuthIcon],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   host: {
@@ -17,12 +17,26 @@ import { RouterLink } from '@angular/router';
   },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Header {
-  protected screenService = inject(ScreenService);
+export class Header implements OnDestroy {
   protected menuService = inject(MenuService);
+
+  protected readonly isMobile = signal(true);
+
+  private readonly _mobileQuery: MediaQueryList;
+  private readonly _mobileQueryListener: () => void;
 
   constructor() {
     this.menuService.items = MenuItems;
+    const media = inject(MediaMatcher);
+
+    this._mobileQuery = media.matchMedia(`(max-width: ${LARGE_SCREEN_BREAKPOINT}px)`);
+    this.isMobile.set(this._mobileQuery.matches);
+    this._mobileQueryListener = () => this.isMobile.set(this._mobileQuery.matches);
+    this._mobileQuery.addEventListener('change', this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this._mobileQuery.removeEventListener('change', this._mobileQueryListener);
   }
 
   mouseLeave() {
