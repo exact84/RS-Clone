c661f9b270612390b60864fa74de8c727a79b8be
NMakarevich <nikoladze529@mail.ru>
2025-09-06 19:28:03 +0300

    feat: add restoring form from localStorage

--- src/app/features/forms/signup-form/signup-form.ts
+++ src/app/features/forms/signup-form/signup-form.ts
@@ -4,6 +4,7 @@ import { SignupFormInterface } from '../models/forms.model';
 import { FormField } from '../../../shared/ui/form-field/form-field';
 import { PasswordIcon } from '../../../shared/ui/password-icon/password-icon';
 import { isMatchPasswords } from '../../utils/validators';
+import { SIGNUP_LS_KEY } from '../../../shared/constants/constants';
 
 @Component({
   selector: 'app-signup-form',
@@ -14,6 +15,13 @@ import { isMatchPasswords } from '../../utils/validators';
 export class SignupForm {
   private readonly fb = inject(FormBuilder);
 
+  constructor() {
+    const lsFormData = localStorage.getItem(SIGNUP_LS_KEY);
+    if (lsFormData) {
+      this.signupForm.setValue(JSON.parse(lsFormData));
+    }
+  }
+
   signupForm: FormGroup<SignupFormInterface> = this.fb.nonNullable.group(
     {
       login: ['', [Validators.required, Validators.minLength(5)]],
@@ -22,7 +30,7 @@ export class SignupForm {
       password: ['', [Validators.required]],
       confirmPassword: ['', [Validators.required]],
     },
-    { validators: [isMatchPasswords], updateOn: 'blur' },
+    { validators: [isMatchPasswords] },
   );
 
   showPassword = signal(false);
@@ -56,7 +64,12 @@ export class SignupForm {
     this.showConfirmPassword.update((value) => !value);
   }
 
+  onChange() {
+    localStorage.setItem(SIGNUP_LS_KEY, JSON.stringify(this.signupForm.getRawValue()));
+  }
+
   onSubmit() {
     console.log(this.signupForm.getRawValue());
+    localStorage.removeItem(SIGNUP_LS_KEY);
   }
 }
--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -1,2 +1,3 @@
 export const NO_INDEX = -1;
 export const LARGE_SCREEN_BREAKPOINT = 768;
+export const SIGNUP_LS_KEY = 'sign-up-form';
