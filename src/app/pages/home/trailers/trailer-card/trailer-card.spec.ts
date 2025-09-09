21bb4309e6929f7dd124ba73e9a5f6345cd47d0e
evgeniia-korolova <jkod@ukr.net>
2025-09-09 03:14:57 +0300

    fix: trailer-card.spec.ts

--- src/app/pages/home/trailers/trailer-card/trailer-card.spec.ts
+++ src/app/pages/home/trailers/trailer-card/trailer-card.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TrailerCard } from './trailer-card';
+import { MovieWithTrailer } from '../../../models/movie-with-trailer';
 
 describe('TrailerCard', () => {
   let component: TrailerCard;
@@ -13,6 +14,18 @@ describe('TrailerCard', () => {
 
     fixture = TestBed.createComponent(TrailerCard);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('data', {
+      id: 1,
+      title: 'Test Movie',
+      original_title: 'Test Movie Original',
+      poster_path: '/test.jpg',
+      trailerKey: 'abc123',
+      overview: 'Test overview',
+      release_date: '2025-09-09',
+      vote_average: 8.5,
+      vote_count: 100,
+    } satisfies MovieWithTrailer);
+
     fixture.detectChanges();
   });
 
