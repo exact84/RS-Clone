fbf5457b28db0fbb8e38d274e4d27eb678326fbf
evgeniia-korolova <jkod@ukr.net>
2025-09-10 19:51:39 +0300

    refactor: add ChangeDetection.onPush to tabs components

--- src/app/pages/home/home-tabs/home-tabs.ts
+++ src/app/pages/home/home-tabs/home-tabs.ts
@@ -1,11 +1,12 @@
 import { TitleCasePipe } from '@angular/common';
-import { Component, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-home-tabs',
   imports: [TitleCasePipe],
   templateUrl: './home-tabs.html',
   styleUrl: './home-tabs.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeTabs {
   readonly categories = input<readonly { label: string; value: string }[]>([]);
