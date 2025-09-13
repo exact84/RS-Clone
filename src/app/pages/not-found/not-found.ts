368f6f38254c7caad5288bf22bc308e4678a48ad
NMakarevich <nikoladze529@mail.ru>
2025-09-13 17:27:58 +0300

    feat: add not found page content

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './not-found.html',
   styleUrl: './not-found.scss',
 })
-export class NotFound {}
+export class NotFound {
+  activatedRoute = inject(ActivatedRoute);
+
+  path = this.activatedRoute.snapshot.url.join('/');
+}
