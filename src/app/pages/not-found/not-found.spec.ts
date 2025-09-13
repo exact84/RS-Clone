61d5ec00fa855ef7843a14c749003200b1915d1b
NMakarevich <nikoladze529@mail.ru>
2025-09-13 17:27:58 +0300

    feat: add not found page content

--- src/app/pages/not-found/not-found.spec.ts
+++ src/app/pages/not-found/not-found.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NotFound } from './not-found';
+import { ActivatedRoute } from '@angular/router';
 
 describe('NotFound', () => {
   let component: NotFound;
@@ -9,6 +10,14 @@ describe('NotFound', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NotFound],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: { url: ['not', 'found'] },
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NotFound);
