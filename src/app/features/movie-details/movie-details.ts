3abc1f186283ef43dc3e0c97aae4d3c4b83f7303
Alexey Trukhlyayev <exact84@gmail.com>
2025-09-14 22:08:15 +0500

    fix: fallback & types

--- src/app/features/movie-details/movie-details.ts
+++ src/app/features/movie-details/movie-details.ts
@@ -5,7 +5,6 @@ import { toSignal } from '@angular/core/rxjs-interop';
 import { DetailsCardService } from './services/details-card-service';
 import { MovieDetailsCard } from './movie-details-card/movie-details-card';
 import { ContentDetails } from '../../pages/types/content-details';
-import { TVDetails } from '../../pages/models/tv-details';
 import { SPINNER_PATH } from '../../shared/constants/constants';
 
 @Component({
@@ -21,36 +20,38 @@ export class MovieDetails {
   readonly detailsCardsService = inject(DetailsCardService);
   readonly spinnerPath = SPINNER_PATH;
 
-  fallbackDetails: TVDetails = {
-    media_type: 'tv',
-    id: 0,
-    name: '',
-    original_name: '',
-    poster_path: '',
-    overview: '',
-    first_air_date: '',
-    vote_average: 0,
-    vote_count: 0,
-    original_language: '',
-    genres: [],
-    tagline: '',
-    status: '',
-    homepage: null,
-    number_of_seasons: 0,
-    number_of_episodes: 0,
-    episode_run_time: [],
-    production_companies: [],
-    spoken_languages: [],
-  };
+  // fallbackDetails: TVDetails = {
+  //   media_type: 'tv',
+  //   id: 0,
+  //   name: '',
+  //   original_name: '',
+  //   poster_path: '',
+  //   overview: '',
+  //   first_air_date: '',
+  //   vote_average: 0,
+  //   vote_count: 0,
+  //   original_language: '',
+  //   genres: [],
+  //   tagline: '',
+  //   status: '',
+  //   homepage: null,
+  //   number_of_seasons: 0,
+  //   number_of_episodes: 0,
+  //   episode_run_time: [],
+  //   production_companies: [],
+  //   spoken_languages: [],
+  // };
+
+  // fallback будет в movie-details-card
 
   readonly cardDetails: Signal<ContentDetails | undefined> = toSignal(
     this.detailsCardsService.getMovieDetails(this.id, this.type),
     {
-      initialValue: this.fallbackDetails,
+      initialValue: undefined,
     },
   );
 
-  readonly isLoading = computed(() => this.cardDetails() === this.fallbackDetails);
+  readonly isLoading = computed(() => this.cardDetails() === undefined);
 
   readonly isError = computed(() => this.detailsCardsService.errorSignal());
 }
