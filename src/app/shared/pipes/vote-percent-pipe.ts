4adfe2e7e899e3975cc8eeee57ca4053bf186ca3
evgeniia-korolova <jkod@ukr.net>
2025-09-16 03:54:07 +0300

    feat: add recomendations slider

--- src/app/shared/pipes/vote-percent-pipe.ts
+++ src/app/shared/pipes/vote-percent-pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'votePercentPipe',
 })
 export class VotePercentPipe implements PipeTransform {
-  transform(vote: number | null | undefined): string {
+  transform(vote: number | null): string {
     if (vote == null) return '-';
     return `${Math.round(vote * 10)}%`;
   }
