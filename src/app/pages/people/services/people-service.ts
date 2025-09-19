ef5f4ddec01636d356288e45d20a76ed9de05b2a
evgeniia-korolova <jkod@ukr.net>
2025-09-19 11:31:27 +0300

    fix: remove redundant code

--- src/app/pages/people/services/people-service.ts
+++ src/app/pages/people/services/people-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { PeopleResponse } from '../../models/people/people-response.interface';
 import { ApiErrorService } from '../../../core/services/api-error-service';
 
@@ -12,21 +12,9 @@ export class PeopleService {
   private readonly apiError = inject(ApiErrorService);
   readonly errorSignal = signal<string | null>(null);
 
-  // getPeople(page: number = 1): Observable<PeopleResponse> {
-  //   const start = performance.now();
-  //   return this.http.get<PeopleResponse>(`/person/popular?page=${page}`).pipe(
-  //     map((response) => response),
-  //     this.apiError.handleApiError(this.errorSignal, 'Failed to load people'),
-  //   );
-  // }
   getPeople(page = 1): Observable<PeopleResponse> {
-    const start = performance.now();
-    return this.http.get<PeopleResponse>(`/person/popular?page=${page}`).pipe(
-      tap(() => {
-        const end = performance.now();
-        console.log(`🕒 API response time: ${(end - start).toFixed(2)}ms`);
-      }),
-      this.apiError.handleApiError(this.errorSignal, 'Failed to load people'),
-    );
+    return this.http
+      .get<PeopleResponse>(`/person/popular?page=${page}`)
+      .pipe(this.apiError.handleApiError(this.errorSignal, 'Failed to load people'));
   }
 }
