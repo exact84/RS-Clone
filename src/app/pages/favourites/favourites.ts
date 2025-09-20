1336a3108881c28b400f299266550bd1903aae9b
NMakarevich <nikoladze529@mail.ru>
2025-09-20 16:41:47 +0300

    feat: add layout for favourites page

--- src/app/pages/favourites/favourites.ts
+++ src/app/pages/favourites/favourites.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FavouritesStore } from '../../shared/store/states/favourites.state';
+import { Dispatcher } from '@ngrx/signals/events';
+import { favouritesEvents } from '../../shared/store/events/favourites.events';
+import { SliderCard } from '../home/slider-card/slider-card';
+import { ContentCard } from '../types/content-card';
 
 @Component({
   selector: 'app-favourites',
-  imports: [],
+  imports: [SliderCard],
   templateUrl: './favourites.html',
   styleUrl: './favourites.scss',
 })
-export class Favourites {}
+export class Favourites {
+  readonly favouritesStore = inject(FavouritesStore);
+  private readonly dispatcher = inject(Dispatcher);
+
+  constructor() {
+    this.dispatcher.dispatch(favouritesEvents.loadFavourites());
+  }
+
+  favourites = this.favouritesStore.favouritesLists;
+
+  deleteFromFavourites(id: string, item: ContentCard) {
+    const contentId = `${item.media_type}/${item.id}`;
+    this.dispatcher.dispatch(favouritesEvents.deleteFromFavourites({ id, contentId }));
+  }
+}
