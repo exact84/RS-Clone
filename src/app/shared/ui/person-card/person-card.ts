5c3b816491b65257759f959bba3c543d999a4d17
evgeniia-korolova <jkod@ukr.net>
2025-09-21 00:12:29 +0300

    feat: add redirect from movie-details card to person-details card

--- src/app/shared/ui/person-card/person-card.ts
+++ src/app/shared/ui/person-card/person-card.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
 import { actorPhotoURL, FALLBACK_ACTOR } from '../../constants/constants';
 import { Person } from '../../../pages/models/people/person.interface';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-person-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './person-card.html',
   styleUrl: './person-card.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
