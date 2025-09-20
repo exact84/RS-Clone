b992263132506c4e8990bbda15896c42436b800f
NMakarevich <nikoladze529@mail.ru>
2025-09-20 16:30:17 +0300

    feat: add method for loading card

--- src/app/pages/favourites/api/favourites.service.ts
+++ src/app/pages/favourites/api/favourites.service.ts
@@ -1,12 +1,21 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FavouritesInterface } from '../models/favourites';
+import { MediaType } from '../../../features/movie-details/types/media-type';
+import { ContentCard } from '../../types/content-card';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FavouritesService {
   private readonly http = inject(HttpClient);
+
+  getContent(id: number, type: MediaType) {
+    return this.http
+      .get<ContentCard>(`/${type}/${id}?language=en-US`)
+      .pipe(map((card) => ({ ...card, media_type: type }) as ContentCard));
+  }
 
   getLabels() {
     return this.http.get<Omit<FavouritesInterface, 'ids' | 'userId'>[]>('/favourites/labels', {
