8fa0323f38f8831f29a0f4e0496cdabdb8845bfd
Alexey Trukhlyayev <exact84@gmail.com>
2025-09-20 16:03:40 +0500

    feat: add footer

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../../shared/header/header';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [Header, RouterOutlet],
+  imports: [Header, RouterOutlet, Footer],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
