ec52247d933a330dd62b6058790b96a44bd88265
NMakarevich <nikoladze529@mail.ru>
2025-09-21 20:05:56 +0300

    refactor: update styles

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   BASE_URL: 'https://api.themoviedb.org/3',
   BASE_URL_BACKEND: 'https://movie-db-backend.up.railway.app/api',
-  API_KEY: '',
+  API_KEY: 'd4ef1072498b844582d90d6ade6ff65f',
 };
