797a4c4d0db284d2af9a769efcadc19b525a7043
NMakarevich <nikoladze529@mail.ru>
2025-09-24 14:40:50 +0300

    feat: add button component

---
+++ src/app/shared/ui/button/button.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'button[app-button]',
+  imports: [],
+  templateUrl: './button.html',
+  styleUrl: './button.scss',
+})
+export class Button {}
