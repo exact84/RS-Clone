dfe04ffb8c4ada1da99c00e7d7829326b38bccaf
evgeniia-korolova <jkod@ukr.net>
2025-09-25 16:53:48 +0300

    feat: add mock-data for testing services in home page

---
+++ src/app/shared/testing/mocks/movies.mock.ts
@@ -0,0 +1,30 @@
+import { ContentCard } from '../../../pages/types/content-card';
+
+export const mockMovies: ContentCard[] = [
+  {
+    id: 872_585,
+    title: 'Oppenheimer',
+    original_title: 'Oppenheimer',
+    poster_path: '/ptpr0kGAckfQkJeJIt8st5dglHc.jpg',
+    genre_ids: [18, 36],
+    media_type: 'movie',
+    overview:
+      'The story of J. Robert Oppenheimer’s role in the development of the atomic bomb during World War II.',
+    release_date: '2023-07-19',
+    vote_average: 8.1,
+    vote_count: 7000,
+  },
+  {
+    id: 940_551,
+    title: 'The Equalizer 3',
+    original_title: 'The Equalizer 3',
+    poster_path: '/b0Ej6fnXAP8fK75hlyi2jKqdhHz.jpg',
+    genre_ids: [28, 53],
+    media_type: 'movie',
+    overview:
+      'Robert McCall finds himself at home in Southern Italy, but discovers his friends are under the control of local crime bosses.',
+    release_date: '2023-08-30',
+    vote_average: 7.3,
+    vote_count: 1800,
+  },
+];
---
+++ src/app/shared/testing/mocks/trailer-response.mock.ts
@@ -0,0 +1,6 @@
+export const mockTrailerResponse = {
+  results: [
+    { type: 'Trailer', site: 'YouTube', key: 'abc123' },
+    { type: 'Teaser', site: 'YouTube', key: 'zzz999' },
+  ],
+};
