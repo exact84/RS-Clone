c854a4fdd3da8dc07bd1f6338e2aa8f70195a69f
evgeniia-korolova <jkod@ukr.net>
2025-09-26 20:54:56 +0300

    feat: add tests to http-interceptor.spec.ts

--- src/app/core/interceptors/http-interceptor.spec.ts
+++ src/app/core/interceptors/http-interceptor.spec.ts
@@ -1,14 +1,58 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpInterceptorFn,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 
 import { httpInterceptor } from './http-interceptor';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { environment } from '../../../environments/environment';
+import { MockEnvironment } from '../utils/mock-environment.type';
 
 describe('httpInterceptor', () => {
+  let httpMock: HttpTestingController;
+  let http: HttpClient;
+
   const interceptor: HttpInterceptorFn = (request, next) =>
     TestBed.runInInjectionContext(() => httpInterceptor(request, next));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(withInterceptors([httpInterceptor])),
+        provideHttpClientTesting(),
+      ],
+    });
+
+    httpMock = TestBed.inject(HttpTestingController);
+    http = TestBed.inject(HttpClient);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should prepend BASE_URL_BACKEND for backend endpoints', () => {
+    http.get('/auth').subscribe();
+
+    const request = httpMock.expectOne(`${environment.BASE_URL_BACKEND}/auth`);
+    expect(request.request.method).toBe('GET');
+    request.flush({});
+  });
+
+  it('should append api_key for TMDB endpoints in dev', () => {
+    const mockEnvironment = environment as MockEnvironment;
+    mockEnvironment.production = false;
+    mockEnvironment.API_KEY = 'mock-key';
+
+    http.get('/movie/popular').subscribe();
+
+    const expectedUrl = `${environment.BASE_URL}/movie/popular?api_key=mock-key`;
+    const request = httpMock.expectOne(expectedUrl);
+    expect(request.request.method).toBe('GET');
+    request.flush({});
   });
 
   it('should be created', () => {
