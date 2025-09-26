975c4c2d9db73ae4916cf0f03c5f888676d7c803
Alexey Trukhlyayev <exact84@gmail.com>
2025-09-26 23:34:32 +0500

    feat: add input for search result page

--- src/app/pages/movies/movie-search/movie-search.ts
+++ src/app/pages/movies/movie-search/movie-search.ts
@@ -1,4 +1,13 @@
-import { Component, inject, Input, OnInit, signal } from '@angular/core';
+import {
+  Component,
+  inject,
+  Input,
+  OnInit,
+  signal,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { MovieSearchService } from './services/movie-search-service';
 import { MovieCard } from '../../models/movie-card';
 import { SliderCard } from '../../home/slider-card/slider-card';
@@ -10,7 +19,7 @@ import { Spinner } from '../../../shared/ui/spinner/spinner';
   templateUrl: './movie-search.html',
   styleUrl: './movie-search.scss',
 })
-export class MovieSearch implements OnInit {
+export class MovieSearch implements OnInit, AfterViewInit {
   searchService = inject(MovieSearchService);
   @Input() query = '';
 
@@ -18,12 +27,21 @@ export class MovieSearch implements OnInit {
   loadingState = signal(false);
   errorState = signal<string | null>(null);
 
+  @ViewChild('searchInput') searchInput!: ElementRef<HTMLInputElement>;
+
+  ngAfterViewInit() {
+    this.searchInput.nativeElement.focus();
+  }
   ngOnInit() {
-    if (this.query) {
+    if (this.query) this.submitSearch(this.query);
+  }
+
+  submitSearch(query: string) {
+    if (query) {
       this.loadingState.set(true);
       this.errorState.set(null);
 
-      this.searchService.getSearchedMovies(this.query).subscribe({
+      this.searchService.getSearchedMovies(query).subscribe({
         next: (response) => {
           this.movies.set(response.results);
           this.loadingState.set(false);
