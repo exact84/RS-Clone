0d7c56a17d1afaa8d407e0914de4f7a5f0d03e2e
evgeniia-korolova <jkod@ukr.net>
2025-09-27 20:15:46 +0300

    feat: add now-plaing movies component

--- src/app/features/movie-details/services/recommendations-service.ts
+++ src/app/features/movie-details/services/recommendations-service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { Observable, map } from 'rxjs';
-import { MovieCard } from '../../../pages/models/movie-card';
 import { ApiErrorService } from '../../../core/services/api-error-service';
+import { ContentCard } from '../../../pages/types/content-card';
 
 @Injectable({
   providedIn: 'root',
@@ -12,19 +12,25 @@ export class RecommendationsService {
   private readonly apiError = inject(ApiErrorService);
   readonly errorSignal = signal<string | null>(null);
 
-  getRecommendations(movieId: number, mediaType: 'movie' | 'tv'): Observable<MovieCard[]> {
-    return this.http.get<{ results: MovieCard[] }>(`/${mediaType}/${movieId}/recommendations`).pipe(
-      map((response) =>
-        response.results
-          .filter(
-            (item) => item.poster_path && (item.media_type === 'movie' || item.media_type === 'tv'),
-          )
-          .map((item) => ({
-            ...item,
-            media_type: item.media_type as 'movie' | 'tv',
-          })),
-      ),
-      this.apiError.handleApiError(this.errorSignal, 'Failed to load recommendations'),
-    );
+  getRecommendations(movieId: number, mediaType: 'movie' | 'tv'): Observable<ContentCard[]> {
+    return this.http
+      .get<{ results: ContentCard[] }>(`/${mediaType}/${movieId}/recommendations`)
+      .pipe(
+        map((response) =>
+          response.results
+            .filter(
+              (item) =>
+                item.poster_path && (item.media_type === 'movie' || item.media_type === 'tv'),
+            )
+            .map(
+              (item) =>
+                ({
+                  ...item,
+                  media_type: item.media_type as 'movie' | 'tv',
+                }) as ContentCard,
+            ),
+        ),
+        this.apiError.handleApiError(this.errorSignal, 'Failed to load recommendations'),
+      );
   }
 }
