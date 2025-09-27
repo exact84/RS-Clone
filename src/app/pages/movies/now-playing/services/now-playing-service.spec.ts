734c3b60e45d7a880710a40efffea04b5e951327
evgeniia-korolova <jkod@ukr.net>
2025-09-27 20:18:37 +0300

    fix: now-playing-service.spec.ts

--- src/app/pages/movies/now-playing/services/now-playing-service.spec.ts
+++ src/app/pages/movies/now-playing/services/now-playing-service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NowPlayingService } from './now-playing-service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('NowPlayingService', () => {
   let service: NowPlayingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(NowPlayingService);
   });
 
