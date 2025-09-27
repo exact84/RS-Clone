b62328c7f50df9dbf5e0a9b5954bd1ece85edacf
Alexey Trukhlyayev <exact84@gmail.com>
2025-09-28 01:33:40 +0500

    feat: add Sentry for error monitoring, logging and tracing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import * as Sentry from '@sentry/angular';
+
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { version } from '../package.json';
+
+Sentry.init({
+  dsn: 'https://1c43437d9526f50f6650e5a311dee80c@o4510092284067840.ingest.de.sentry.io/4510092317098064',
+  release: `RS-clone@${version}`,
+  sendDefaultPii: true,
+  enableLogs: true,
+  sampleRate: 1,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.consoleLoggingIntegration({ levels: ['log', 'warn', 'error'] }),
+  ],
+  tracesSampleRate: 0.2,
+});
 
 // eslint-disable-next-line unicorn/prefer-top-level-await
 bootstrapApplication(App, appConfig).catch((error) => console.error(error));
