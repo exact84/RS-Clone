751c02f01caddb4a8cc7e58e641f8aeafd50611e
NMakarevich <nikoladze529@mail.ru>
2025-09-29 00:22:14 +0300

    refactor: add scroll loading

--- src/app/pages/movies/movie-search/movie-search.ts
+++ src/app/pages/movies/movie-search/movie-search.ts
@@ -8,24 +8,34 @@ import {
   ViewChild,
   ElementRef,
   ChangeDetectionStrategy,
+  viewChild,
 } from '@angular/core';
 import { MovieSearchService } from './services/movie-search-service';
-import { MovieCard } from '../../models/movie-card';
 import { SliderCard } from '../../home/slider-card/slider-card';
 import { Spinner } from '../../../shared/ui/spinner/spinner';
+import { Button } from '../../../shared/ui/button/button';
+import { BaseMovieListComponent } from '../base-movie-component';
+import { map, Observable } from 'rxjs';
+import { ContentCard } from '../../types/content-card';
 
 @Component({
   selector: 'app-movie-search',
-  imports: [SliderCard, Spinner],
+  imports: [SliderCard, Spinner, Button],
   templateUrl: './movie-search.html',
   styleUrl: './movie-search.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MovieSearch implements OnInit, AfterViewInit {
+export class MovieSearch extends BaseMovieListComponent implements OnInit, AfterViewInit {
   searchService = inject(MovieSearchService);
   @Input() query = '';
 
-  movies = signal<MovieCard[]>([]);
+  readonly scrollAnchor = viewChild('scrollAnchor', { read: ElementRef });
+
+  constructor() {
+    super();
+    this.setScrollAnchorSignal(this.scrollAnchor);
+  }
+
   loadingState = signal(false);
   errorState = signal<string | null>(null);
 
@@ -36,6 +46,12 @@ export class MovieSearch implements OnInit, AfterViewInit {
   }
   ngOnInit() {
     if (this.query) this.submitSearch(this.query);
+  }
+
+  protected fetchMovies(genreId: number, page: number): Observable<ContentCard[]> {
+    return this.searchService
+      .getSearchedMovies(this.query, page)
+      .pipe(map((response) => response.results.map((card) => ({ ...card, media_type: 'movie' }))));
   }
 
   submitSearch(query: string) {
